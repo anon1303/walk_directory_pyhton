544f66c3332a187d9a96f44b75ea346ee3127639
anon1303 <jomari.daison@gmail.com>
2022-04-16 10:23:21 +0800

    update script(prints  stat.S_IFSOCK, stat.S_IFLNK etc..)

--- walkv4.py
+++ walkv4.py
@@ -61,7 +61,31 @@ def get_path_stat(path):
     # [ CHR  => S_IFCHR  ],
     # [ FIFO => S_IFIFO  ]
 
+def walktree(top, callback):
+    '''recursively descend the directory tree rooted at top,
+       calling the callback function for each regular file'''
 
+    for f in os.listdir(top):
+        pathname = os.path.join(top, f)
+        mode = os.lstat(pathname).st_mode
+
+
+
+
+        if S_ISDIR(mode):
+            # It's a directory, recurse into it
+            walktree(pathname, callback)
+        elif S_ISREG(mode):
+
+            # It's a file, call the callback function
+            callback(pathname)
+        else:
+            # Unknown file type, print a message
+            print('Skipping %s' % pathname)
+
+def visitfile(file):
+    # print('visiting', file)
+    pass
 
 def get_list(path):
 
@@ -93,6 +117,7 @@ if __name__ == '__main__':
 
     try:
         PATH = get_path()
+        walktree(PATH['<PATHNAME>'], visitfile)
         main(PATH)
         
     except OSError:
